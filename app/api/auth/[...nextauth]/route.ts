d21d258c97710555b3acf81b209c2fa6d3ca8f3b
poiu <ponpottboomk@gmail.com>
2025-01-02 14:42:38 +0700

    Add NextAuth integration with credentials provider for user authentication

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,74 @@
+import NextAuth, { NextAuthOptions, Session } from 'next-auth'
+import CredentialsProvider from 'next-auth/providers/credentials'
+import { PrismaClient } from '@prisma/client'
+import { JWT } from 'next-auth/jwt'
+import bcrypt from 'bcrypt'
+import { PrismaAdapter } from '@auth/prisma-adapter'
+import { NextApiHandler } from 'next'
+
+
+const prisma = new PrismaClient()
+
+declare module 'next-auth' {
+  interface Session {
+    user: {
+      id: string;
+      name?: string | null;
+      email?: string | null;
+      image?: string | null;
+    }
+  }
+}
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        email: { label: 'Email', type: 'email', placeholder: 'john@doe.com' },
+        password: { label: 'Password', type: 'password' },
+      },
+      async authorize(credentials) {
+        if (!credentials) return null
+        const user = await prisma.user.findUnique({
+          where: { email: credentials.email },
+        })
+
+        if (
+          user &&
+          (await bcrypt.compare(credentials.password, user.password))
+        ) {
+          return {
+            id: user.id.toString(),
+            name: user.name,
+            email: user.email
+          }
+        } else {
+          throw new Error('Invalid email or password')
+        }
+      },
+    })
+  ],
+  adapter: PrismaAdapter(prisma),
+  session: {
+    strategy: 'jwt',
+  },
+  callbacks: {
+    jwt: async ({ token, user }) => {
+      if (user) {
+        token.id = user.id
+      }
+      return token
+    },
+    session: async ({ session, token }: { session: Session, token: JWT }) => {
+      if (session.user) {
+        session.user.id = token.id as string
+      }
+      return session
+    }
+  },
+}
+
+const handler: NextApiHandler = NextAuth(authOptions)
+
+export { handler as GET, handler as POST }
